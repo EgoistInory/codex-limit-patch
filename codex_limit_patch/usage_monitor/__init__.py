fa435001432f2bc687424dfc52c3d955dca0f965
huxiaocong <sabershilang@126.com>
2026-07-11 10:30:09 +0800

    Add local quota alert evaluation

--- codex_limit_patch/usage_monitor/__init__.py
+++ codex_limit_patch/usage_monitor/__init__.py
@@ -1,8 +1,11 @@
+from .alerts import UsageAlert, evaluate_alerts
 from .models import AccountSnapshot, ModelUsage, QuotaWindow, load_snapshots
 
 __all__ = [
     "AccountSnapshot",
     "ModelUsage",
     "QuotaWindow",
+    "UsageAlert",
+    "evaluate_alerts",
     "load_snapshots",
 ]
